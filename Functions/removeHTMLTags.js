4c884105d66a111100bfb84169b5ba3505306b11
Praveen B <Praveen.B@sapiens.com>
2024-05-04 21:53:44 +0530

    added_dev_framework_enhancement

--- Functions/removeHTMLTags.js
+++
@@ -1,9 +0,0 @@
-// Function to remove HTML tags from a string
-function removeHtmlTags(str) {
-  if (typeof str !== 'string') {
-    return str; 
-  }
-  return str.replace(/<[^>]*>/g, ''); 
-}
-
-module.exports= removeHtmlTags
