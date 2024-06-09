3d5deb758ab7cf7b047d59f1e62bb24ad02b18c9
Praveen B <Praveen.B@sapiens.com>
2024-06-09 20:30:09 +0530

    Added Rss feed logic with clean up task

--- utils/coindeskArticleScrapper.js
+++ utils/coindeskArticleScrapper.js
@@ -9,28 +9,95 @@ const scrapeArticle = async (timeout, url) => {
     setTimeout(
       async () => {
         try {
-          const scrappedData = await page.evaluate(async () => {
-            const title =
-              document.querySelector('.at-headline')?.innerText || ''
-            const description =
-              document.querySelector('.at-subheadline')?.innerText || ''
-            const metaData = document.querySelectorAll(
-              '[data-submodule-name="composer-content"]',
+          let mainPageData = {}
+          try {
+            await page.waitForSelector('div.at-authors', { timeout: 30000 })
+            mainPageData = await page.evaluate(async () => {
+              const title =
+                document.querySelector('.at-headline')?.innerText || ''
+
+              const metaDataElement = document.querySelector('.at-row')
+
+              let metaDataText = ''
+
+              if (metaDataElement) {
+                authorText =
+                  document.querySelector('.at-authors')?.innerText || ''
+                createdAt =
+                  document.querySelector('.at-created.label-with-icon')
+                    ?.innerText || ''
+                updatedAt =
+                  document.querySelector('.at-updated')?.innerText || ''
+                authorLink = metaDataElement.querySelector('a')
+                  ? metaDataElement.querySelector('a').href
+                  : null
+
+                metaDataText = `\n Author: ${authorText}\n Created at: ${createdAt}\n Updated at: ${updatedAt}\n Authorlink: ${authorLink}`
+              }
+
+              const subheadline =
+                document.querySelector('.at-subheadline')?.innerText || ''
+
+              const descriptionElements = document.querySelectorAll(
+                '[data-submodule-name="composer-content"]',
+              )
+
+              const authorElements = document.querySelectorAll(
+                '[class^="at-author-block"]',
+              )
+
+              return {
+                title,
+                subheadline,
+                metaData: metaDataText,
+                description: Array.from(descriptionElements)
+                  .map((element) => element.innerText)
+                  .join(' '),
+                author: Array.from(authorElements)
+                  .map((element) => element.innerText)
+                  .join(' '),
+              }
+            })
+          } catch (error) {
+            console.warn('Selector "div.at-authors" not found, skipping...')
+          }
+
+          let iframedData = {}
+
+          try {
+            const iframeElement = await page.waitForSelector(
+              'iframe[title="X Post"]',
+              { timeout: 20000 },
             )
-            const author =
-              document.querySelector('[class^="at-author-block"]')?.innerText ||
-              ''
+            const iframe = await iframeElement.contentFrame()
 
-            return {
-              title,
-              description,
-              metaData:
-                metaData[0]?.innerText + ' \n' + metaData[1]?.innerText || '',
-              author,
-            }
-          })
+            iframedData = await iframe.evaluate(() => {
+              const tweetElements = document.querySelectorAll(
+                '[role="article"]',
+              )
+              const links = document.querySelectorAll('a')
 
-          resolve(scrappedData)
+              return {
+                tweet: Array.from(tweetElements)
+                  .map((tweet) => tweet.innerText)
+                  .join(' '),
+                link: Array.from(links)
+                  .map((link) => link.href)
+                  .join(' '),
+              }
+            })
+          } catch (error) {
+            console.warn(
+              'Iframe with title "X Post" not found, skipping iframe data...',
+            )
+          }
+
+          const combinedData = {
+            ...mainPageData,
+            ...iframedData,
+          }
+
+          resolve(combinedData)
         } catch (error) {
           reject(error)
         }
@@ -45,28 +112,19 @@ const extractArticleLinks = async () => {
   const { page } = await getPage()
   // Navigate the page to a URL
   await page.goto(coinDeskUrl)
+  // Extract links from the RSS feed
+  const articleLinks = await page.evaluate(() => {
+    const items = document.querySelectorAll('item')
+    return Array.from(items)
+      .map((item) => {
+        const linkElement = item.querySelector('link')
+        return linkElement ? linkElement.textContent : null
+      })
+      .filter((link) => link !== null)
+  })
 
-  // Close the dialog using DOM manipulation
-  await page.click('[id="CybotCookiebotDialogBodyButtonAccept"]')
-  // Get all matching articles
-  const articles = await page.$$('.card-title')
-  console.log(`Found ${articles.length} articles.`)
-  if (articles.length > 0) {
-    // Extract the hrefs of the parent anchor articles from child articles
-    let articleLinks = await page.evaluate(() => {
-      const articleTags = document.querySelectorAll('a .card-title') // Replace with your child element selector
-      return Array.from(articleTags)
-        .map((articleTag) => {
-          const parentAnchor = articleTag.closest('a') // Find the closest parent anchor element
-          return parentAnchor ? parentAnchor.href : null
-        })
-        .filter((href) => href !== null) // Filter out null values
-    })
-
-    console.log('article Links:', articleLinks)
-    return articleLinks
-  }
-  return []
+  console.log('article Links:', articleLinks)
+  return articleLinks
 }
 
 module.exports = { scrapeArticle, extractArticleLinks }
--- utils/pageUtil.js
+++ utils/pageUtil.js
@@ -5,7 +5,11 @@ const getPage = async (singleton = true, headless = false) => {
   if (singleton && page && browser) {
     return { page, browser }
   }
-  browser = await puppeteer.launch({ headless, timeout: 60000 })
+  browser = await puppeteer.launch({
+    headless,
+    timeout: 60000,
+    protocolTimeout: 120000,
+  })
   page = await browser.newPage()
 
   // Set screen size
